1f23f709f120a28210a8d0b49fc32a42b4a18215
Daniel-Collins97 <daniel.collins@routematch.com>
2020-03-09 12:16:55 +0000

    Added error handling so that service does not shut down when network call receives an error

--- queries.js
+++ queries.js
@@ -9,9 +9,10 @@ const pool = new Pool({
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM "Users"', (error, results) => {
     if (error) {
-      throw error
+      response.status(500).json(error)
+    } else {
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 
@@ -20,9 +21,10 @@ const getUsersById = (request, response) => {
 
   pool.query('SELECT * FROM "Users" WHERE id = $1', [id], (error, results) => {
     if (error) {
-      throw error
+      response.status(500).json(error)
+    } else {
+      response.status(200).json(results.rows)
     }
-    response.status(200).json(results.rows)
   })
 }
 
@@ -31,9 +33,10 @@ const createUser = (request, response) => {
 
   pool.query('INSERT INTO "Users" ("firstName", "lastName", "age", "height", "weight", "position") VALUES ($1, $2, $3, $4, $5, $6)', [firstName, lastName, age, height, weight, position], (error) => {
     if (error) {
-      throw error
+      response.status(500).json(error)
+    } else {
+      response.status(201).send(`User added`)
     }
-    response.status(201).send(`User added`)
   })
 }
 
@@ -43,9 +46,10 @@ const updateUser = (request, response) => {
 
   pool.query('UPDATE "Users" SET "firstName" = $1 WHERE id =$2', [firstName, id], (error) => {
     if (error) {
-      throw error;
+      response.status(500).json(error)
+    } else {
+      response.status(200).send(`User modified with ID: ${id}`);
     }
-    response.status(200).send(`User modified with ID: ${id}`);
   })
 }
 
@@ -54,9 +58,10 @@ const deleteUser = (request, response) => {
 
   pool.query('DELETE FROM "Users" WHERE id = $1', [id], (error) => {
     if (error) {
-      throw error;
+      response.status(500).json(error)
+    } else {
+      response.status(200).send(`User deleted with ID: ${id}`);
     }
-    response.status(200).send(`User deleted with ID: ${id}`);
   })
 }
 
